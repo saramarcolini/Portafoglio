d622b2863c503ca1446b53a4d9d83a0220286669
kleinleon <120364151+kleinleon@users.noreply.github.com>
2023-01-28 11:03:43 +0100

    File Main
    Gets the csv file from yahoo Finance

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+def getFile(url):
+    header = {'Connection': 'keep-alive',
+            'Expires': '-1',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
+            }
+    website = requests.get(url, headers=header)
+    return website.text.split('\n')
+
+def main():
+    csv = getFile("https://query1.finance.yahoo.com/v7/finance/download/PIRC.MI?period1=1642923403&period2=1674459403&interval=1d&events=history&includeAdjustedClose=true")
+    for line in csv:
+        print(line)
+
+if __name__ == "__main__":
+    main()
