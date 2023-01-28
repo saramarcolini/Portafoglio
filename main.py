50373f3ca35d947fd74bad803c22202b80dea824
RiccardoVillardi <120247714+RiccardoVillardi@users.noreply.github.com>
2023-01-28 16:37:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import pandas
+import json
 
 def getFile(url):
     header = {'Connection': 'keep-alive',
@@ -8,12 +10,21 @@ def getFile(url):
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
             }
     website = requests.get(url, headers=header)
-    return website.text.split('\n')
+    return website.text
+
+def convert_to_json(csv):
+    df = pandas.read_csv(csv)
+    data_dict = df.to_dict()
+    json_data = json.dumps(data_dict)
+    with open("data.json", "w") as f:
+        f.write(json_data)
+    print("Conversion to JSON done.")
 
 def main():
     csv = getFile("https://query1.finance.yahoo.com/v7/finance/download/PIRC.MI?period1=1642923403&period2=1674459403&interval=1d&events=history&includeAdjustedClose=true")
-    for line in csv:
-        print(line)
+    with open("file.csv", "w") as f:
+        f.write(csv)
+    convert_to_json("file.csv")
 
 if __name__ == "__main__":
     main()
